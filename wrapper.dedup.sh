4e2ec83ae168686a4275deabf2271e8185277590
johncheney <71104613+johncheney@users.noreply.github.com>
2021-11-03 00:25:04 -0700

    adding deduper, wrapper, output counts files

---
+++ wrapper.dedup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash 
+#SBATCH --partition=bgmp        ### Partition 
+#SBATCH --job-name=dedup         ### Job Name 
+#SBATCH --output=dedupy%j    ### File in which to store job output  
+#SBATCH --time=10:00:00       
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=8 
+#SBATCH --account=bgmp 
+
+module load samtools 
+ 
+samtools sort C1_SE_uniqAlign.sam -o C1_SE_uniqAlign.sorted.sam 
+
+#conda activate bgmp_py39
+
+/usr/bin/time -v ./cheney_deduper.py \
+-f C1_SE_uniqAlign.sorted.sam \
+-u STL96.txt \
+-o C1_SE_uniqAlign.deduped.output.sam
+
+wc -l C1_SE_uniqAlign.sorted.sam
+wc -l C1_SE_uniqAlign.deduped.output.sam
+
